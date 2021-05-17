d772c8d7fc6dde1c15bb5df447c6c2182462a361
DiegoPadilla01 <82250804+DiegoPadilla01@users.noreply.github.com>
2021-05-16 22:36:58 -0500

    Add files via upload

---
+++ Facturacion.java
@@ -0,0 +1,18 @@
+package facturacion;
+
+/**
+ *
+ * @author ANDRES
+ */
+public class Facturacion {
+
+    /**
+     * @param args the command line arguments
+     */
+   
+    public static void main(String[] args) {
+       new Interfaz_principal().setVisible(true);
+    }
+
+   
+}
